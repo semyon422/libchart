6743415b9b26ee108db4575dfa3904cd5d64a391
semyon422 <semyon422@users.noreply.github.com>
2022-05-20 21:20:21 +0500

    Normalscore v2.1: multiple hit windows

--- libchart/normalscore2.lua
+++ libchart/normalscore2.lua
@@ -2,8 +2,6 @@ local erfunc = require("libchart.erfunc")
 
 local normalscore = {}
 
-normalscore.t_L = -1
-normalscore.t_R = 1
 normalscore.samples_count = 0
 normalscore.hit_count = 0
 normalscore.miss_count = 0
@@ -17,29 +15,68 @@ normalscore.score_adjusted = 0
 normalscore.ratio_score = 0
 normalscore.miss_addition = 0
 
-function normalscore:new()
-	return setmetatable({}, {__index = self})
+function normalscore:new(ranges)
+	assert(type(ranges) == "table", "ranges must be a table")
+	local ns = {
+		ranges = ranges,
+		hit_counts = {},
+		samples_counts = {},
+	}
+	for i = 1, #ranges do
+		ns.hit_counts[i] = 0
+		ns.samples_counts[i] = 0
+	end
+	return setmetatable(ns, {__index = self})
+end
+
+function normalscore:eq1i(sigma, i)
+	local N = self.samples_count
+	local range = self.ranges[i]
+	local t_L, t_R = range[1], range[2]
+	local H_i = self.hit_counts[i]
+	local N_i = self.samples_counts[i]
+
+	return N_i / (2 * N) * (
+		erfunc.erf(t_R / (sigma * math.sqrt(2))) - erfunc.erf(t_L / (sigma * math.sqrt(2)))
+	) - H_i / N
 end
 
 function normalscore:eq1(sigma)
-    return 1 / 2 * (
-		erfunc.erf(self.t_R / (sigma * math.sqrt(2))) - erfunc.erf(self.t_L / (sigma * math.sqrt(2)))
-	) - self.hit_count / (self.hit_count + self.miss_count)
+	local sum = 0
+	for i = 1, #self.ranges do
+		sum = sum + self:eq1i(sigma, i)
+	end
+	return sum
+end
+
+function normalscore:eq1di(sigma, i)
+	local range = self.ranges[i]
+	local t_L, t_R = range[1], range[2]
+
+	return 1 / (sigma ^ 2 * math.sqrt(2 * math.pi)) * (
+		-t_R * math.exp(-(t_R / (sigma * math.sqrt(2))) ^ 2) +
+		t_L * math.exp(-(t_L / (sigma * math.sqrt(2))) ^ 2)
+	)
 end
 
 function normalscore:eq1d(sigma)
-    return 1 / (sigma ^ 2 * math.sqrt(2 * math.pi)) * (
-        -self.t_R * math.exp(-(self.t_R / (sigma * math.sqrt(2))) ^ 2) +
-        self.t_L * math.exp(-(self.t_L / (sigma * math.sqrt(2))) ^ 2)
-    )
+	local sum = 0
+	for i = 1, #self.ranges do
+		sum = sum + self:eq1di(sigma, i)
+	end
+	return sum
 end
 
-function normalscore:press(delta_time)
-	local t_L = self.t_L
-	local t_R = self.t_R
+function normalscore:press(delta_time, range_index)
+	local range = self.ranges[range_index]
+	assert(range, "range_index out of range")
+	local t_L, t_R = range[1], range[2]
+
 	self.samples_count = self.samples_count + 1
+	self.samples_counts[range_index] = self.samples_counts[range_index] + 1
 	if delta_time >= t_L and delta_time <= t_R then
 		self.hit_count = self.hit_count + 1
+		self.hit_counts[range_index] = self.hit_counts[range_index] + 1
 		self.mean_sum = self.mean_sum + delta_time
 		self.variance_sum = self.variance_sum + delta_time ^ 2
 		self.mean = self.mean_sum / self.hit_count
@@ -50,34 +87,45 @@ function normalscore:press(delta_time)
 end
 
 function normalscore:update()
-	local t_L = self.t_L
-	local t_R = self.t_R
 	local N = self.hit_count + self.miss_count
 
 	if self.miss_count > 0 then
-		local miss_ratio = self.miss_count / N
+		local ranges = self.ranges
 		local tau_0 = erfunc.erfinv(self.hit_count / N)
-		if t_L + t_R == 0 then
-			self.ratio_score = t_R / (tau_0 * math.sqrt(2))
-			self.miss_addition = (t_R / tau_0) ^ 2 * (miss_ratio + 2 * tau_0 / math.sqrt(math.pi) * math.exp(-tau_0 ^ 2)) / 2
-		else
-			local x
-			local sigma_m = (t_R - t_L) / (2 * tau_0 * math.sqrt(2))
-			local i = 1
-			repeat
-				x = sigma_m
-				sigma_m = sigma_m - self:eq1(sigma_m) / self:eq1d(sigma_m)
-				i = i + 1
-			until x == sigma_m or i > 20
-			self.ratio_score = sigma_m
 
+		local sum = 0
+		local sum_weights = 0
+		for i = 1, #ranges do
+			local range = ranges[i]
+			local t_L, t_R = range[1], range[2]
+			sum = sum + (t_R - t_L) / (2 * tau_0 * math.sqrt(2)) * self.hit_counts[i]
+			sum_weights = sum_weights + self.hit_counts[i]
+		end
+		local sigma_m = sum / sum_weights
+
+		local x
+		local k = 1
+		repeat
+			x = sigma_m
+			sigma_m = sigma_m - self:eq1(sigma_m) / self:eq1d(sigma_m)
+			k = k + 1
+		until x == sigma_m or k > 20
+		self.ratio_score = sigma_m
+
+		local sum_NdT = 0
+		for i = 1, #ranges do
+			local range = ranges[i]
+			local t_L, t_R = range[1], range[2]
 			local tau_L = t_L / (sigma_m * math.sqrt(2))
 			local tau_R = t_R / (sigma_m * math.sqrt(2))
-			self.miss_addition = sigma_m ^ 2 * (miss_ratio + 1 / math.sqrt(math.pi) * (
-				tau_R * math.exp(-tau_R ^ 2) - tau_L * math.exp(-tau_L ^ 2)
-			))
+			sum_NdT = sum_NdT +
+				(tau_R * math.exp(-tau_R ^ 2) - tau_L * math.exp(-tau_L ^ 2)) /
+				math.sqrt(math.pi) * self.samples_counts[i]
 		end
+
+		self.miss_addition = sigma_m ^ 2 * (self.miss_count + sum_NdT) / N
 	end
+
 	local score_squared = self.variance_sum / N + self.miss_addition
 
 	self.score_squared = score_squared
