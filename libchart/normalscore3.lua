d504d5610fc3210eed21d2496844e3d53804447c
semyon422 <semyon422@users.noreply.github.com>
2023-08-04 21:36:33 +0500

    Fix bugs with {x, x} and {nil, nil} ranges

--- libchart/normalscore3.lua
+++ libchart/normalscore3.lua
@@ -26,9 +26,14 @@ function normalscore:new()
 end
 
 function normalscore:eq1i(sigma, name)
-	local N = self.samples_count
 	local range = self.ranges[name]
-	local t_L, t_R = range[1] - self.mean, range[2] - self.mean
+	local t_L, t_R = range[1], range[2]
+	if t_L == t_R then  -- both nil or equal
+		t_L, t_R = self:get_max_range()
+	end
+	t_L, t_R = t_L - self.mean, t_R - self.mean
+
+	local N = self.samples_count
 	local H_i = self.hit_counts[name]
 	local N_i = self.samples_counts[name]
 
@@ -64,6 +69,27 @@ function normalscore:get_range(range_name)
 	return ranges[range_name]
 end
 
+function normalscore:get_max_range()
+	local ranges = self.ranges
+
+	local l, r = math.huge, -math.huge
+	for _, range in pairs(ranges) do
+		if range[1] then
+			l = math.min(l, range[1])
+			r = math.max(r, range[2])
+		end
+	end
+
+	return l, r
+end
+
+function normalscore:unpack_range(range)
+	if range[1] then
+		return range[1], range[2]
+	end
+	return self:get_max_range()
+end
+
 function normalscore:hit(range_name, delta_time)
 	local range = self:get_range(range_name)
 
@@ -93,47 +119,67 @@ function normalscore:miss(range_name)
 end
 
 function normalscore:update()
-	local N = self.samples_count
 	local mean = self.mean
+	local H, M, N = self.hit_count, self.miss_count, self.samples_count
 
-	if self.miss_count > 0 and self.hit_count > 0 then
-		local tau_t_0 = erfunc.erfinv(self.hit_count / N)
-
-		local sum = 0
-		local sum_weights = 0
-		for name, range in pairs(self.ranges) do
-			local t_L, t_R = range[1], range[2]
-			sum = sum + (t_R - t_L) / (2 * tau_t_0 * math.sqrt(2)) * self.hit_counts[name]
-			sum_weights = sum_weights + self.hit_counts[name]
-		end
-		local sigma_m = sum / sum_weights
-
-		local x
-		local k = 1
-		repeat
-			x = sigma_m
-			sigma_m = sigma_m - self:eq1(sigma_m)
-			k = k + 1
-		until x == sigma_m or k > 20
-		self.ratio_score = sigma_m
-
-		local sum_NdT = 0
-		for name, range in pairs(self.ranges) do
-			local t_L, t_R = range[1], range[2]
-			local tau_L = (t_L - mean) / (sigma_m * math.sqrt(2))
-			local tau_R = (t_R - mean) / (sigma_m * math.sqrt(2))
-			sum_NdT = sum_NdT +
-				(tau_R * math.exp(-tau_R ^ 2) - tau_L * math.exp(-tau_L ^ 2)) /
-				math.sqrt(math.pi) * self.samples_counts[name]
-		end
-
-		self.miss_addition = sigma_m ^ 2 * (self.miss_count + sum_NdT) / N
-	elseif self.miss_count > 0 and self.hit_count == 0 then
-		self.ratio_score = math.huge
-		self.miss_addition = math.huge
+	if M == 0 and H == 0 then
+		self.score_squared = 0
+		self.score = 0
+		return
 	end
 
-	local score_squared = self.hit_count / math.max(N, 1) * (self.variance - mean ^ 2) + self.miss_addition
+	if M == 0 and H > 0 then
+		self.score_squared = self.variance - mean ^ 2
+		self.score = math.sqrt(self.score_squared)
+		return
+	end
+
+	if M > 0 and H == 0 then
+		self.score_squared = math.huge
+		self.score = math.huge
+		return
+	end
+
+	local minL, maxR = self:get_max_range()
+	if minL == maxR then
+		self.score_squared = math.huge
+		self.score = math.huge
+		return
+	end
+
+	local eiHN = erfunc.erfinv(H / N)
+
+	local sum = 0
+	local sum_weights = 0
+	for name, range in pairs(self.ranges) do
+		local t_L, t_R = self:unpack_range(range)
+		sum = sum + (t_R - t_L) / (2 * eiHN * math.sqrt(2)) * self.hit_counts[name]
+		sum_weights = sum_weights + self.hit_counts[name]
+	end
+	local sigma_m = sum / sum_weights
+
+	local x
+	local k = 1
+	repeat
+		x = sigma_m
+		sigma_m = sigma_m - self:eq1(sigma_m)
+		k = k + 1
+	until x == sigma_m or k > 20
+	self.ratio_score = sigma_m
+
+	local sum_NdT = 0
+	for name, range in pairs(self.ranges) do
+		local t_L, t_R = self:unpack_range(range)
+		local tau_L = (t_L - mean) / (sigma_m * math.sqrt(2))
+		local tau_R = (t_R - mean) / (sigma_m * math.sqrt(2))
+		sum_NdT = sum_NdT +
+			(tau_R * math.exp(-tau_R ^ 2) - tau_L * math.exp(-tau_L ^ 2)) /
+			math.sqrt(math.pi) * self.samples_counts[name]
+	end
+
+	self.miss_addition = sigma_m ^ 2 * (self.miss_count + sum_NdT) / N
+
+	local score_squared = H / N * (self.variance - mean ^ 2) + self.miss_addition
 
 	self.score_squared = score_squared
 	self.score = math.sqrt(score_squared)
