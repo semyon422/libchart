0712a65ac36764f62a99d6b64143bdbe2370cb0f
semyon422 <semyon422@users.noreply.github.com>
2024-02-29 23:28:06 +0500

    Update normalscore3_test.lua

--- libchart/normalscore3_test.lua
+++ libchart/normalscore3_test.lua
@@ -1,7 +1,6 @@
-package.loaded["libchart.erfunc"] = require("erfunc")
-local normalscore = require("normalscore3")
+local normalscore = require("libchart.normalscore3")
 
-local ns = normalscore:new()
+local test = {}
 
 local function norm_values(n, mu, sigma)
 	math.randomseed(0)
@@ -28,35 +27,38 @@ local function norm_values(n, mu, sigma)
 	return values
 end
 
-local function press(t, range, name)
-	if t >= range[1] and t <= range[2] then
-		ns:hit(name, t)
-		return
+function test.basic(t)
+	local ns = normalscore:new()
+
+	local function press(t, range, name)
+		if t >= range[1] and t <= range[2] then
+			ns:hit(name, t)
+			return
+		end
+		ns:miss(name)
 	end
-	ns:miss(name)
+
+	local sigma = 0.02
+	local mu = 1000
+
+	local values1 = norm_values(1e4, mu, sigma)
+	local range1 = {-0.02 + mu, 0.02 + mu}
+	for _, v in ipairs(values1) do
+		press(v, range1, "range1")
+	end
+
+	local values2 = norm_values(1e4, mu, sigma)
+	local range2 = {-0.03 + mu, 0.03 + mu}
+	for _, v in ipairs(values2) do
+		press(v, range2, "range2")
+	end
+
+	ns:update()
+
+	t:lt(math.abs(ns.score - sigma) / sigma, 0.01)
+	t:assert(ns.ranges.range1[1] > 0)
+	t:assert(ns.ranges.range1[2] > 0)
 end
-
-local sigma = 0.02
-local mu = 1000
-
-local values1 = norm_values(1e6, mu, sigma)
-local range1 = {-0.02 + mu, 0.02 + mu}
-for _, v in ipairs(values1) do
-	press(v, range1, "range1")
-end
-
-local values2 = norm_values(1e6, mu, sigma)
-local range2 = {-0.03 + mu, 0.03 + mu}
-for _, v in ipairs(values2) do
-	press(v, range2, "range2")
-end
-
-ns:update()
-
-assert(math.abs(ns.score - sigma) / sigma < 0.01)
-assert(ns.ranges.range1[1] > 0 and ns.ranges.range1[2] > 0)
-
-----------------------------
 
 local function is_inf(score)
 	return math.abs(score) == math.huge
@@ -70,47 +72,47 @@ local function is_valid(score)
 	return not is_inf(score) and not is_nan(score)
 end
 
-do
+function test.test1(t)
 	local ns = normalscore:new()
 	ns:hit("A", 0.1)
 	ns:update()
-	assert(is_valid(ns.score))
+	t:assert(is_valid(ns.score))
 end
 
-do
+function test.test2(t)
 	local ns = normalscore:new()
 	ns:miss("A")
 	ns:update()
-	assert(is_inf(ns.score))
+	t:assert(is_inf(ns.score))
 end
 
-do
+function test.test3(t)
 	local ns = normalscore:new()
 	ns:hit("A", 0.1)
 	ns:miss("A")
 	ns:update()
-	assert(is_valid(ns.score))
+	t:assert(is_valid(ns.score))
 end
 
-do
+function test.test4(t)
 	local ns = normalscore:new()
 	ns:hit("A", 0.1)
 	ns:hit("A", 0.11)
 	ns:miss("B")
 	ns:update()
-	assert(is_valid(ns.score))
+	t:assert(is_valid(ns.score))
 end
 
-do
+function test.test5(t)
 	local ns = normalscore:new()
 	ns:hit("A", 0.1)
 	ns:hit("B", 0.11)
 	ns:miss("B")
 	ns:update()
-	assert(is_valid(ns.score))
+	t:assert(is_valid(ns.score))
 end
 
-do
+function test.test6(t)
 	local ns = normalscore:new()
 	ns:hit("A", 0.1)
 	ns:hit("A", 0.11)
@@ -118,11 +120,11 @@ do
 	ns:hit("B", 0.13)
 	ns:miss("B")
 	ns:update()
-	assert(is_valid(ns.score))
+	t:assert(is_valid(ns.score))
 end
 
 -- a case with inf/nan sigma_m, fix it later
-do
+function test.test7(t)
 	local ns = normalscore:new()
 
 	ns.ranges.a = {0.1223646190068, -0.010260595034254}
@@ -134,5 +136,7 @@ do
 	ns.mean_sums.a = 0.011442789872754 * ns.hit_counts.a
 
 	local a, b = ns:eq1i(0.00041512071994903, "a")
-	print(a, b, a / b)
+	-- print(a, b, a / b)
 end
+
+return test
