2645ce71bd92a3f730dc728f7b047b2a1b4aaee9
Thetan <83221369+Thetan-ILW@users.noreply.github.com>
2025-05-28 14:58:20 +0300

    Add calc_msd_rate to calc MSD for a specific rate

--- libchart/minacalc.lua
+++ libchart/minacalc.lua
@@ -25,6 +25,7 @@ local bit = require("bit")
 ---@field create_calc fun(): minacalc.CalcHandle
 ---@field destroy_calc fun(handle: minacalc.CalcHandle)
 ---@field calc_msd fun(calc: minacalc.CalcHandle, rows: minacalc.NoteInfo[], num_rows: integer, keycount: integer): minacalc.MsdForAllRates
+---@field calc_msd_rate fun(calc: minacalc.CalcHandle, rows: minacalc.NoteInfo[], num_rows: integer, music_rate: number, keycount: integer): minacalc.Ssr
 ---@field calc_ssr fun(calc: minacalc.CalcHandle, rows: minacalc.NoteInfo[], num_rows: integer, music_rate: number, score_goal: number, keycount: integer): minacalc.Ssr
 
 ffi.cdef [[
@@ -58,6 +59,7 @@ ffi.cdef [[
 	void destroy_calc(CalcHandle *calc);
 
 	MsdForAllRates calc_msd(CalcHandle *calc, const NoteInfo *rows, size_t num_rows, const unsigned int keycount);
+	Ssr calc_msd_rate(CalcHandle *calc, const NoteInfo *rows, size_t num_rows, float music_rate, unsigned keycount);
 	Ssr calc_ssr(CalcHandle *calc, NoteInfo *rows, size_t num_rows, float music_rate, float score_goal, const unsigned int keycount);
 ]]
 
@@ -68,16 +70,15 @@ local calc_handle = lib.create_calc()
 
 local minacalc = {}
 
+---@type minacalc.Ssr
 local empty_ssr = ffi.new("Ssr")
+---@type minacalc.MsdForAllRates
+local empty_msds = ffi.new("MsdForAllRates")
 
 ---@param notes {time: number, column: integer}[]
----@param columns integer
----@param rate number
----@param accuracy number?
----@return minacalc.Ssr
-function minacalc.calc(notes, columns, rate, accuracy)
-	accuracy = accuracy or 0.93
-
+---@return minacalc.NoteInfo[] rows
+---@return number num_rows
+function minacalc.get_rows(notes)
 	---@type {[number]: integer}
 	local rows_map = {}
 
@@ -101,15 +102,74 @@ function minacalc.calc(notes, columns, rate, accuracy)
 		return a.rowTime < b.rowTime
 	end)
 
-	---@type minacalc.NoteInfo[]
-	local _rows = ffi.new("NoteInfo[?]", #rows, rows)
+	return ffi.new("NoteInfo[?]", #rows, rows), #rows
+end
 
+---@param notes {time: number, column: integer}[]
+---@param columns integer
+---@param base_rate minacalc.Ssr
+---@return number[] rate_multipliers
+function minacalc.calc_rate_multipliers(notes, columns, base_rate)
+	local rows, num_rows = minacalc.get_rows(notes)
+	columns = math.ceil(columns / 2) * 2
+
+	---@type minacalc.Ssr[]
+	local msds = empty_msds.msds
+
+	local ok, err = pcall(lib.calc_msd, calc_handle, rows, num_rows, columns)
+	if ok then
+		msds = err.msds
+	end
+
+	local rate_multipliers = {}
+
+	for i = 0, 13, 1 do
+		table.insert(rate_multipliers, msds[i].overall / base_rate.overall)
+	end
+
+	return rate_multipliers
+end
+
+---@param notes {time: number, column: integer}[]
+---@param columns integer
+---@param rate number
+---@return minacalc.Ssr
+function minacalc.calc(notes, columns, rate)
+	local rows, num_rows = minacalc.get_rows(notes)
 	columns = math.ceil(columns / 2) * 2
 
 	local ssr = empty_ssr
 
 	-- C++ exception
-	local ok, err = pcall(lib.calc_ssr, calc_handle, _rows, #rows, rate, accuracy, columns)
+	local ok, err = pcall(lib.calc_msd_rate, calc_handle, rows, num_rows, rate, columns)
+	if ok then
+		ssr = err
+	end
+
+	return {
+		overall = ssr.overall,
+		stream = ssr.stream,
+		jumpstream = ssr.jumpstream,
+		handstream = ssr.handstream,
+		stamina = ssr.stamina,
+		jackspeed = ssr.jackspeed,
+		chordjack = ssr.chordjack,
+		technical = ssr.technical,
+	}
+end
+
+---@param notes {time: number, column: integer}[]
+---@param columns integer
+---@param rate number
+---@param accuracy number?
+---@return minacalc.Ssr
+function minacalc.calc_ssr(notes, columns, rate, accuracy)
+	local rows, num_rows = minacalc.get_rows(notes)
+	columns = math.ceil(columns / 2) * 2
+
+	local ssr = empty_ssr
+
+	local ok, err = pcall(lib.calc_ssr, calc_handle, rows, num_rows, rate, accuracy, columns)
 	if ok then
 		ssr = err
 	end
